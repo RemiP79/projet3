ee7b8abe4ee44ded32fbc6b43b918f852e752ed0
Remi P <perez.remi@gmail.com>
2023-01-23 08:31:41 +0100

    modale ajout photo

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,7 +1,7 @@
 
 /**
  * 
- * @param {object} data affichage des informations issues du backend
+ * @param {object} data affichage des projet sur la page d'accueil, issus du backend
  */
 const createWorks = (data)  => { 
   let figure = document.createElement("figure");
@@ -59,17 +59,121 @@ supprGallerie.addEventListener("click", () => {
   alert ("en cours de réalisation");
 });
 
-// ===========================création de la boite de selection de catégories dans la modale
+/*const deleteBouton =(e) => {  
+  if(figure.id= e.target.id){
+    fig.remove();
+  }
+}*/
+
+const ajouterPhoto = document.getElementById("ajouterPhoto");
+ajouterPhoto.addEventListener("click", () => { 
+    document.getElementById("modale").style.display = "flex";
+    document.getElementById("winModale").style.display = "none";
+    document.getElementById("winModaleAdd").style.display = "flex";
+})
+
+/**
+ * Construction Fenetre winmodale2 pour ajouter un objet
+ */
+const windowModaleAdd = (data) => {
+  const winModaleAdd = document.getElementById("winModaleAdd");
+  const div=document.createElement("div");
+  div.setAttribute("id","iconesModifImage");
+
+  const iconeRetour=document.createElement("i");
+  iconeRetour.setAttribute("class","fa-solid fa-arrow-left");
+  iconeRetour.addEventListener("click", () => {
+    document.getElementById("modale").style.display = "flex";
+    document.getElementById("winModale").style.display = "flex";
+    document.getElementById("winModaleAdd").style.display = "none";
+  })
+
+  const iconeClose=document.createElement("i");
+  iconeClose.setAttribute("class","fa-solid fa-xmark");
+
+  const titre = document.createElement("p");
+  titre.textContent="Ajout photo";
+  titre.setAttribute("id", "titreModale2");
+
+  const div2 = document.createElement("div");
+  div2.setAttribute("class","cadreWinmod");
+
+  const div1=document.createElement("div");
+  div1.setAttribute("id","cadreAjoutImg");
+  const imageModif= document.createElement("i");
+  imageModif.setAttribute("class","fa-regular fa-image");
+  imageModif.setAttribute("id","imageModif");
 
 
-/*for(let elem of data) {
+  const labelButtonModif=document.createElement("label");
+  labelButtonModif.setAttribute("for", "ajoutPhoto");
+  const buttonModif=document.createElement("button");
+  buttonModif.setAttribute("type","submit");
+  buttonModif.setAttribute("value","ajoutPhoto");
+  buttonModif.setAttribute("name","ajoutPhoto");
+  buttonModif.setAttribute("id","ajoutPhoto");
+  buttonModif.textContent ="+ Ajouter Photo";
+
+  const infoTypeImg = document.createElement("p");
+  infoTypeImg.textContent="jpg,png 4mo max";
+
+  const formulaire=document.createElement("form");
+  formulaire.setAttribute("method","post");
+  formulaire.setAttribute("action","AA");
+  formulaire.setAttribute("id","formAjoutPhoto");
+
+  const br=document.createElement("br");
+
+  const labelTitre=document.createElement("label");
+  labelTitre.setAttribute("for", "labelTitreAjout");
+  labelTitre.setAttribute("class", "labelAjout");
+  labelTitre.textContent="Titre";
+
+  const inputText=document.createElement("input");
+  inputText.setAttribute("type", "textAjoutPhoto");
+  inputText.setAttribute("name", "textAjoutPhoto");
+  inputText.setAttribute("id", "textAjoutPhoto");
+  inputText.setAttribute("class", "inputTextAjout");
+  inputText.setAttribute("required", "required");
+  inputText.setAttribute("placeholder", "Donnez un nom à votre projet");
+
+  const labelOption=document.createElement("label");
+  labelOption.setAttribute("for","selectCategory");
+  labelOption.textContent = "Catégorie";
+  labelOption
+  const optionCategory=document.createElement("select");
+  optionCategory.setAttribute("id", "catges");
+  const selectVide=document.createElement("option");
+    selectVide.setAttribute("value","");   
+    selectVide.textContent="";    
+    optionCategory.append(selectVide);
+
+  for(let elem of data) {  
     let cat = document.createElement("option");
-    document.querySelector("catges").appendChild(cat);  // ==> ==> INSERER LE CATGES AU BON ENDROIT DANS LE HTML
+    optionCategory.appendChild(cat);  // ==> ==> INSERER LE CATGES AU BON ENDROIT DANS LE HTML
     cat.value = elem.id;
     cat.textContent = elem.name;
-  }*/
-  
+  }
 
+  const trait=document.createElement("div");
+  trait.setAttribute("class","trait");
+
+  const labelAddPhotoValider=document.createElement("label");
+  labelAddPhotoValider.setAttribute("for", "validModifPhoto");
+  const addPhotoValider=document.createElement("button");
+  addPhotoValider.setAttribute("type","submit");
+  addPhotoValider.setAttribute("value","Valider");
+  addPhotoValider.setAttribute("name","validModifPhoto");
+  addPhotoValider.setAttribute("id","validModifPhoto");
+  addPhotoValider.textContent="Valider";
+
+  div.append(iconeRetour,iconeClose)
+  div1.append(imageModif,labelButtonModif,buttonModif,infoTypeImg);
+  div2.append(div1,formulaire,trait);
+  formulaire.append(labelTitre,inputText,labelOption,optionCategory);
+  winModaleAdd.append(div,titre,div2,labelAddPhotoValider,addPhotoValider);
+
+}
 
 /**
  * 
@@ -157,6 +261,9 @@ const clickBouton = (e) => {
 
 
 
+
+
+
 /**
  * @param {objet} //Affichage des projets (création d'une fonction getData qui est asynchrone)
  */
@@ -179,7 +286,9 @@ const getData = async() => {
         }            
     }    
     createWorksByCat(categorys);     
-    isConnected ();    
+    isConnected ();  
+    windowModaleAdd(categorys);
+      
   }    
   catch (error) {
     alert (error);
@@ -203,19 +312,31 @@ const isConnected = () => {
     }    
     // Fermeture modale
   document.getElementById("x").addEventListener("click", () => {      
-    document.getElementById("modale").style.display = "none"; 
+   
+    document.getElementById("modale").style.display = "none";
+    document.getElementById("winModale").style.display = "none";
+    document.getElementById("winModaleAdd").style.display = "none"; //document.getElementById("modale").style.display = "none";
+    //document.getElementById("winModaleAdd").classList.toggle("notVisible"); 
   });
   window.addEventListener('click', outsideClick);      
 };
 
 function outsideClick(e){
   if(e.target == modale){
-    modale.style.display = 'none';
+    document.getElementById("modale").style.display = "none";
+    document.getElementById("winModale").style.display = "none";
+    document.getElementById("winModaleAdd").style.display = "none";
+    
+    //modale.style.display = 'none';
+    //document.getElementById("winModaleAdd").classList.toggle("notVisible"); 
   }
 }
 
 const openModal = () =>{  
- document.getElementById("barreNoire").style.display = "flex";
+ //document.getElementById("barreNoire").style.display = "flex";
  document.getElementById("modale").style.display = "flex";
+ document.getElementById("winModale").style.display = "flex";
+ document.getElementById("winModaleAdd").style.display = "none";
+
 }
 
